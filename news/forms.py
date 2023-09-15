05a56475eff8f1fbeb74dfc8b5a3a4dba8204cf8
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-09-15 01:24:45 -0300

    Req7 fix

--- news/forms.py
+++ news/forms.py
@@ -13,21 +13,10 @@ class CategoryForm(forms.ModelForm):
 
 
 class NewsForm(forms.ModelForm):
-    class Meta:
-        fields = [
-            "title",
-            "content",
-            "author",
-            "created_at",
-            "image",
-            "categories",
-        ]
-        model = News
-
     author = forms.ModelChoiceField(
         label="Autoria", queryset=Users.objects.all()
     )
-    title = forms.CharField(label="Título", max_length=200)
+    title = forms.CharField(label="Título", max_length=150)
     categories = forms.ModelMultipleChoiceField(
         label="Categorias",
         widget=forms.CheckboxSelectMultiple,
@@ -35,10 +24,14 @@ class NewsForm(forms.ModelForm):
     )
     content = forms.CharField(
         label="Conteúdo",
-        max_length=200,
+        max_length=150,
         widget=forms.Textarea(attrs={"type": "textarea"}),
     )
     created_at = forms.DateField(
         label="Criado em", widget=forms.DateTimeInput(attrs={"type": "date"})
     )
     image = forms.FileField(label="URL da Imagem", required=False)
+
+    class Meta:
+        fields = "__all__"
+        model = News
