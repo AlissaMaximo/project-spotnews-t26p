ffb70273d0ed159fecd60a09628cbfcd51d5d924
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-09-14 23:48:59 -0300

    Req 6 fix

--- news/models/category_model.py
+++ news/models/category_model.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Categories(models.Model):
-    name = models.CharField(max_length=200, null=False)
+    name = models.CharField(max_length=200, blank=False)
 
     def __str__(self):
         return self.name
